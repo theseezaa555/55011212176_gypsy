5377a2abe6f2ff73c9b2438aa99f74b906e0c423
iStudents <istudents@iClassrooms-iMac-10.local>
2015-01-30 16:17:48 +0700

    week3_calculator

---
+++ calculator.playground/section-1.swift
@@ -0,0 +1,48 @@
+
+class TipCalculator {
+    
+    // 2
+    let total: Double
+    let taxPct: Double
+    init (total:Double,taxPct:Double){
+        self.total=total
+        self.taxPct=taxPct
+    }
+    // 3
+   
+    func process() -> Double{
+        let total1 = total + taxPct
+        return total1
+    }
+    func process1() -> Double{
+        let total2 = total - taxPct
+        return total2
+    }
+    func process2() -> Double{
+        let total3 = total * taxPct
+        return total3
+    }
+    func process3() -> Double{
+        let total4 = total / taxPct
+        return total4
+    }
+    func process4() -> Double{
+        let total5 = total % taxPct
+        return total5
+    }
+  
+   
+
+    
+    
+    
+
+}
+// 6
+let tipCalc = TipCalculator(total: 10, taxPct: 10)
+tipCalc.process()
+tipCalc.process1()
+tipCalc.process2()
+tipCalc.process3()
+tipCalc.process4()
+
