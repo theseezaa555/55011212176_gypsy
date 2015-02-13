1fca158835300ab6074b404428b595d0f9cf8f77
iStudents <istudents@iClassrooms-iMac.local>
2015-02-13 17:06:10 +0700

    week5_tableview

---
+++ week5_tableview/week5_tableview/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  week5_tableview
+//
+//  Created by iStudents on 2/13/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    let cellIdentifier = "cellIdentifier"
+    var tableData = ["basic16","door24","basic17"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.TableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var TableView: UITableView!
+    
+    func numberOfSectionsInTableView(TableView: UITableView!) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView!.dequeueReusableCellWithIdentifier(self.cellIdentifier) as UITableViewCell
+        
+        cell.textLabel!.text = self.tableData[indexPath.row]
+        
+        var imageName = UIImage(named: tableData[indexPath.row])
+        cell.imageView!.image = imageName
+        
+        //println("\(imageName)")
+        
+        return cell
+    }
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let alert = UIAlertController(title: "Item selected", message: "You selected item \(indexPath.row)", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (alert:UIAlertAction!) in println("An alert of type \(alert.style.hashValue) was tapped!")
+        }))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
+}
+
---
+++ week5_tableview/week5_tableviewTests/week5_tableviewTests.swift
@@ -0,0 +1,36 @@
+//
+//  week5_tableviewTests.swift
+//  week5_tableviewTests
+//
+//  Created by iStudents on 2/13/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class week5_tableviewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
