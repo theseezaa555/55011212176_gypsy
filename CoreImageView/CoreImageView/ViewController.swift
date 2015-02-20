ccc9a7d6084654c5292123a3d775e2afbff54883
iStudents <istudents@iClassrooms-iMac-2.local>
2015-02-20 17:17:54 +0700

    week6_coreImage

---
+++ CoreImageView/CoreImageView/ViewController.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  CoreImageView
+//
+//  Created by iStudents on 2/20/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+import AssetsLibrary
+
+class ViewController: UIViewController ,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+
+    
+    @IBOutlet weak var UIImageCiew: UIImageView!
+    var context: CIContext!
+    var filter: CIFilter!
+    var beginImage: CIImage!
+    var orientation: UIImageOrientation = .Up
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fileURL = NSBundle.mainBundle().URLForResource("image", withExtension: "png")
+        beginImage = CIImage(contentsOfURL: fileURL)
+        
+        filter = CIFilter(name: "CISepiaTone")
+        filter.setValue(beginImage, forKey: kCIInputImageKey)
+        filter.setValue(0.5, forKey: kCIInputIntensityKey)
+        let outputImage = filter.outputImage
+        context = CIContext(options: nil)
+        
+        println("\(beginImage)")
+        let cgimg = context.createCGImage(outputImage, fromRect: outputImage.extent())
+        let newImage = UIImage(CGImage: cgimg)
+        self.UIImageCiew.image = newImage
+        self.logAllFilter()
+        
+        
+    }
+
+    @IBAction func loadbut(sender: AnyObject) {
+        let pickerC = UIImagePickerController()
+        pickerC.delegate = self
+        self.presentViewController(pickerC, animated: true, completion: nil)
+        
+    }
+    @IBAction func savebut(sender: AnyObject) {
+        let imageTosave = filter.outputImage
+        
+        let softwareContext = CIContext(options: [kCIContextUseSoftwareRenderer:true])
+        
+        let cgimg = softwareContext.createCGImage(imageTosave, fromRect: imageTosave.extent())
+        
+        let library = ALAssetsLibrary()
+        library.writeImageToSavedPhotosAlbum(cgimg, metadata: imageTosave.properties(), completionBlock: nil)
+        
+        
+        
+        
+    }
+    
+    @IBOutlet weak var amountSlider: UISlider!
+    @IBAction func amountSliderValueChanged(sender: UISlider) {
+        let sliderValue = sender.value
+        
+        let outputImage = self.oldPhoto(beginImage, withAmount: sliderValue)
+        
+        let cgimg = context.createCGImage(outputImage, fromRect: outputImage.extent())
+        
+        let newImage = UIImage(CGImage: cgimg, scale: 1, orientation: orientation)
+        
+        self.UIImageCiew.image = newImage
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+    func imagePickerController(picker : UIImagePickerController!, didFinishPickingMediaWithInfo info: NSDictionary!){
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        let gotImage = info[UIImagePickerControllerOriginalImage] as UIImage
+        
+        beginImage = CIImage(image: gotImage)
+        orientation = gotImage.imageOrientation
+        filter.setValue(beginImage, forKey: kCIInputImageKey)
+        self.amountSliderValueChanged(amountSlider)
+        
+    }
+    func oldPhoto(img: CIImage,withAmount intensity:Float) ->CIImage{
+        let sepia = CIFilter(name: "CISepiaTone")
+        sepia.setValue(img, forKey: kCIInputImageKey)
+        sepia.setValue(intensity, forKey: "inputIntensity")
+        
+        let random = CIFilter(name: "CIRandomGenerator")
+        
+        let lighten = CIFilter(name: "CIColorControls")
+        lighten.setValue(random.outputImage, forKey: kCIInputImageKey)
+        lighten.setValue(1 - intensity, forKey: "inputBrightness")
+        lighten.setValue(0, forKey: "inputSaturation")
+        
+        let croppedImage = lighten.outputImage.imageByCroppingToRect(beginImage.extent())
+        
+        let composite = CIFilter(name: "CIHardLightBlendMode")
+        composite.setValue(sepia.outputImage, forKey: kCIInputImageKey)
+        composite.setValue(croppedImage, forKey: kCIInputBackgroundImageKey)
+        
+        let vignette = CIFilter(name: "CIVignette")
+        vignette.setValue(composite.outputImage, forKey: kCIInputImageKey)
+        vignette.setValue(intensity * 2, forKey: "inputIntensity")
+        vignette.setValue(intensity * 30, forKey: "inputRadius")
+        
+        return vignette.outputImage
+        
+    }
+    func logAllFilter(){
+        let properties = CIFilter.filterNamesInCategory(kCICategoryBuiltIn)
+        print(properties)
+        
+        for filterName: AnyObject in properties {
+            let fltr = CIFilter(name: filterName as String)
+            println(fltr.attributes())
+        }
+    }
+
+}
+
---
+++ week6_quiz/week6_quiz/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  week6_quiz
+//
+//  Created by iStudents on 2/20/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+import AssetsLibrary
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var UIImagecriew2: UIImageView!
+    var context: CIContext!
+    var filter: CIFilter!
+    var beginImage: CIImage!
+    var orientation: UIImageOrientation = .Up
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fileURL = NSBundle.mainBundle().URLForResource("image", withExtension: "png")
+        beginImage = CIImage(contentsOfURL: fileURL)
+        
+        filter = CIFilter(name: "CISepiaTone")
+        filter.setValue(beginImage, forKey: kCIInputImageKey)
+        filter.setValue(0.5, forKey: kCIInputIntensityKey)
+        let outputImage = filter.outputImage
+        context = CIContext(options: nil)
+        
+        println("\(beginImage)")
+        let cgimg = context.createCGImage(outputImage, fromRect: outputImage.extent())
+        let newImage = UIImage(CGImage: cgimg)
+        self.UIImagecriew2.image = newImage
+        self.logAllFilter()
+        
+        
+        
+    }
+    func logAllFilter(){
+        let properties = CIFilter.filterNamesInCategory(kCICategoryBuiltIn)
+        print(properties)
+        
+        for filterName: AnyObject in properties {
+            let fltr = CIFilter(name: filterName as String)
+            println(fltr.attributes())
+        }
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+           }
+
+    @IBAction func pre(sender: AnyObject) {
+        
+    }
+
+    @IBAction func next(sender: AnyObject) {
+        
+        for(var i = 1; i < 2; i++) {
+            let fileURL = NSBundle.mainBundle().URLForResource("i", withExtension: "png")
+            beginImage = CIImage(contentsOfURL: fileURL)
+        }
+        
+    }
+}
+
